85941855c3edbda36d7419f47c7bcb7c35dd9e72
Eric Yvorchuk <slim@pcic-0011.pcic.uvic.ca>
2020-07-21 14:48:57 -0700

    Make __init__ empty and set author, email, and version in setup call

--- setup.py
+++ setup.py
@@ -2,18 +2,16 @@ import sys
 
 from setuptools import setup
 
-__author__ = "Eric Yvorchuk"
-__email__ = "eyvorchuk@uvic.ca"
-__version__ = "0.1.0"
+__version__ = (0, 1, 0)
 
 setup(
     name="wps_tools",
     description="A collection of modules used to help create wps processes",
     keywords="wps pywps wps_tools",
-    version=__version__,
+    version='.'.join(str(d) for d in __version__),
     url="https://github.com/pacificclimate/wps-tools",
-    author=__author__,
-    author_email=__email__,
+    author="Eric Yvorchuk",
+    author_email="eyvorchuk@uvic.ca",
     packages=["wps_tools"],
     classifiers=[
         "Programming Language :: Python :: 3.6",
