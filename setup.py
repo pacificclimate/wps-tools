8d05f5f3279ab10909ae2fa2676c3a25e809a610
Eric Yvorchuk <slim@pcic-0011.pcic.uvic.ca>
2020-07-21 13:10:09 -0700

    Use setup from crmprtd rather than sandpiper as template

--- setup.py
+++ setup.py
@@ -1,48 +1,24 @@
-
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""The setup script."""
-
-import os
+import sys
 
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
-REQUIRES_PYTHON = ">=3.5.0"
+__version__ = (0, 1, 0)
 
-about = {}
-with open(os.path.join(here, 'wps-tools', '__version__.py'), 'r') as f:
-    exec(f.read(), about)
-
-classifiers = [
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Intended Audience :: Science/Research',
-    'Operating System :: MacOS :: MacOS X',
-    'Operating System :: POSIX',
-    'Programming Language :: Python',
-    'Natural Language :: English',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Topic :: Scientific/Engineering :: Atmospheric Science',
-    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-]
-
-setup(name='wps-tools',
-      version=about['__version__'],
-      description="A collection of modules used to help create wps processes",
-      long_description=README + '\n\n',
-      long_description_content_type="text/x-rst",
-      author=about['__author__'],
-      author_email=about['__email__'],
-      url='https://github.com/pacificclimate/wps-tools',
-      python_requires=REQUIRES_PYTHON,
-      classifiers=classifiers,
-      license="GNU General Public License v3",
-      keywords='wps pywps wps-tools',
-      packages=find_packages(),
-      include_package_data=True,)
+setup(
+    name="wps-tools",
+    description="A collection of modules used to help create wps processes",
+    keywords="wps pywps wps-tools",
+    version='.'.join(str(d) for d in __version__),
+    url="https://github.com/pacificclimate/wps-tools",
+    author="Eric Yvorchuk",
+    author_email="eyvorchuk@uvic.ca",
+    packages=find_packages(),
+    package_data={
+        'wps-tools': ["wps-tools/*.py"],
+    },
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ]
+)
