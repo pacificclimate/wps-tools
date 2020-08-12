6b0fcd267238a255f283a4f97395baadaefee768
Eric Yvorchuk <slim@pcic-0011.pcic.uvic.ca>
2020-08-12 11:12:30 -0700

    Add logger parameter to log_handler

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,4 +1,5 @@
 import pytest
+import logging
 import os
 from tempfile import TemporaryDirectory
 from pkg_resources import resource_filename
@@ -6,6 +7,14 @@ from wps_tools.utils import log_handler
 from .common import TestResponse
 from .processes.wps_test_process import TestProcess
 
+logger = logging.getLogger()
+
+formatter = logging.Formatter(
+    "%(asctime)s %(levelname)s: wps-tools: %(message)s", "%Y-%m-%d %H:%M:%S"
+)
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 @pytest.mark.parametrize(
     ("message", "process_step"), [("Process completed", "complete")]
@@ -17,7 +26,7 @@ def test_log_handler(message, process_step, log_level, log_file_name, caplog):
     response = TestResponse()
     with TemporaryDirectory() as tmpdir:  # For storing temporary log.txt file
         process.workdir = tmpdir
-        log_handler(process, response, message, process_step, log_level, log_file_name)
+        log_handler(process, response, message, logger, log_level, process_step, log_file_name)
         assert os.path.isfile(os.path.join(tmpdir, log_file_name))
     assert response.message == message
     assert (
