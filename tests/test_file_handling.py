8f6a225184b372df5a1ac4c615c63c3c78ac27d3
cairosanders <cairosanders@hotmail.com>
2020-12-22 12:06:24 -0800

    Move file handling related functions to file_handling.py

---
+++ tests/test_file_handling.py
@@ -0,0 +1,128 @@
+import pytest
+from pkg_resources import resource_filename
+from collections import namedtuple
+from wps_tools.file_handling import (
+    is_opendap_url,
+    get_filepaths,
+    collect_output_files,
+    build_meta_link,
+    copy_http_content,
+    url_handler,
+)
+from wps_tools.testing import (
+    local_path,
+    url_path,
+)
+from netCDF4 import Dataset
+from tempfile import NamedTemporaryFile
+from os import path, remove
+
+NCInput = namedtuple("NCInput", ["url", "file"])
+NCInput.__new__.__defaults__ = ("", "")
+
+nc_file = "gdd_annual_CanESM2_rcp85_r1i1p1_1951-2100.nc"
+
+
+@pytest.mark.online
+@pytest.mark.parametrize(
+    ("url"), [url_path(nc_file, "opendap"), local_path(nc_file),],
+)
+def test_is_opendap_url(url):
+    if "docker" in url:
+        assert is_opendap_url(url)  # Ensure function recognizes this is an opendap file
+    else:
+        assert not is_opendap_url(
+            url
+        )  # Ensure function recognizes this is not an opendap file
+
+
+@pytest.mark.parametrize(
+    ("nc_input"), [[NCInput(file=local_path(nc_file))]],
+)
+def test_get_filepaths_local(nc_input):
+    paths = get_filepaths(nc_input)
+    assert len(paths) == len(nc_input)
+    for path in paths:
+        assert nc_file in path
+
+
+@pytest.mark.online
+@pytest.mark.parametrize(
+    ("nc_input"),
+    [
+        [NCInput(url=url_path(nc_file, "opendap"))],
+        [NCInput(file=local_path(nc_file)), NCInput(url=url_path(nc_file, "opendap")),],
+    ],
+)
+def test_get_filepaths_online(nc_input):
+    for nc in nc_input:
+        if nc.url != "":
+            assert is_opendap_url(nc.url)
+
+    paths = get_filepaths(nc_input)
+    assert len(paths) == len(nc_input)
+    for path in paths:
+        assert nc_file in path
+
+
+@pytest.mark.parametrize(
+    ("varname", "outdir"), [("tiny", resource_filename(__name__, "data"))]
+)
+def test_collect_output_files(varname, outdir):
+    outfiles = collect_output_files(varname, outdir)
+    assert len(outfiles) == 2
+    assert set(outfiles) == set(["tiny_daily_prsn.nc", "tiny_daily_pr.nc"])
+
+
+@pytest.mark.parametrize(
+    ("outfiles", "expected"),
+    [
+        (
+            ["tiny_daily_prsn.nc", "tiny_daily_pr.nc"],
+            ['<file name="tiny_daily_prsn.nc">', '<file name="tiny_daily_pr.nc">'],
+        )
+    ],
+)
+def test_build_meta_link(outfiles, expected):
+    xml = build_meta_link(
+        varname="climo",
+        desc="Climatology",
+        outfiles=outfiles,
+        outdir=resource_filename(__name__, "data"),
+    )
+    assert all([elem in xml for elem in expected])
+
+
+@pytest.mark.online
+@pytest.mark.parametrize(
+    ("http", "expected"),
+    [
+        (
+            url_path(nc_file, "http"),
+            resource_filename(
+                __name__, "data/gdd_annual_CanESM2_rcp85_r1i1p1_1951-2100.nc"
+            ),
+        ),
+    ],
+)
+def test_copy_http_content(http, expected):
+    with NamedTemporaryFile(
+        suffix=".nc", prefix="tmp_copy", dir="/tmp", delete=True
+    ) as tmp_file:
+        tmp_copy = copy_http_content(http, tmp_file)
+        assert dir(Dataset(tmp_copy)) == dir(Dataset(expected))
+
+
+@pytest.mark.online
+@pytest.mark.parametrize(
+    ("url_type", "url"),
+    [("http", url_path(nc_file, "http")), ("opendap", url_path(nc_file, "opendap")),],
+)
+def test_url_handler(url_type, url):
+    processed = url_handler("/tmp", url)
+    if url_type == "http":
+        assert path.exists(processed)
+        remove(processed)
+    elif url_type == "opendap":
+        assert is_opendap_url(processed)
+
---
+++ wps_tools/file_handling.py
@@ -0,0 +1,176 @@
+# Processor imports
+from pywps import FORMATS
+from requests import head, get
+from requests.exceptions import ConnectionError, MissingSchema, InvalidSchema
+from pywps.inout.outputs import MetaLink4, MetaFile
+from pywps.app.exceptions import ProcessError
+
+# Tool import
+from nchelpers import CFDataset
+
+# Library imports
+import os
+from urllib.parse import urlparse
+from urllib.request import urlretrieve
+
+
+def url_handler(workdir, url):
+    """Handles URL based on its type
+
+    A process cannot access to the data from an HTTPServer URL without downloading
+    while OPeNDAP URL can be treated as a filepath.
+    The function returns the given URL if it is an OPeNDAP path.
+    Otherwise, data from the HTTPServer URL are copied to a file created in workdir,
+    and the path to the file is returned.
+
+    Parameters:
+        workdir (str): Path to the workdir
+        url (str): URL to be handled
+
+    Returns:
+        url/local_file (str): URL/filepath with accessible data
+    """
+    if is_opendap_url(url):
+        # OPeNDAP
+        return url
+    elif urlparse(url).scheme and urlparse(url).netloc:
+        # HTTPServer or other
+        local_file = os.path.join(workdir, url.split("/")[-1])
+        urlretrieve(url, local_file)
+        return local_file
+
+
+def is_opendap_url(url):  # From Finch bird
+    """Check if a provided url is an OpenDAP url
+
+    The DAP Standard specifies that a specific tag must be included in the
+    Content-Description header of every request. This tag is one of:
+        "dods-dds" | "dods-das" | "dods-data" | "dods-error"
+    So we can check if the header starts with `dods`.
+    Even then, some OpenDAP servers seem to not include the specified header...
+    So we need to let the netCDF4 library actually open the file.
+
+    Parameters:
+        url (str): Provided url
+
+    Returns:
+        bool: True if url is OpenDAP, False otherwise
+    """
+    try:
+        content_description = head(url, timeout=5).headers.get("Content-Description")
+    except (ConnectionError, MissingSchema, InvalidSchema):
+        return False
+
+    if content_description:
+        return content_description.lower().startswith("dods")
+    else:
+        try:
+            dataset = CFDataset(url)
+        except OSError:
+            return False
+        return dataset.disk_format in ("DAP2", "DAP4")
+
+
+def get_filepaths(nc_input):
+    """Collect list of netcdf file paths
+
+    Each path in nc_input is checked to determine if it is an OpenDAP url. If so,
+    then the url is appended to the path list. If not, then whether or not it's a valid
+    netcdf file is checked. If so, then the path is appended to the list. If not, then 
+    a ProcessError is raised due to an invalid input.
+
+    Parameters:
+        nc_input (pywps.ComplexInput): Object containing local or OpenDAP file paths
+
+    Returns:
+        list: List of filepaths
+    """
+    filepaths = []
+    for path in nc_input:
+        if is_opendap_url(path.url):
+            filepaths.append(path.url)
+        elif path.file.endswith(".nc"):
+            filepaths.append(path.file)
+        else:
+            raise ProcessError(
+                "You must provide a data source (opendap/netcdf). "
+                f"Inputs provided: {nc_input}"
+            )
+    return filepaths
+
+
+def collect_output_files(varname, outdir=os.getcwd()):
+    """Collect output netcdf files
+
+    The given directory is searched for the files that contain
+    the given variable name, indicating that these files were outputted
+    by the same process. Though the default directory is the current directory,
+    it is most often the current WPS process's working directory.
+
+    Parameters:
+        varname (str): Name of variable (must be in file names)
+        outdir (str): Directory containing output files
+
+    Returns:
+        list: List of output files
+    """
+    return [file for file in os.listdir(outdir) if varname in file]
+
+
+def build_meta_link(
+    varname,
+    desc,
+    outfiles,
+    format_name="netCDF",
+    fmt=FORMATS.NETCDF,
+    outdir=os.getcwd(),
+):
+    """Create meta link between output files
+
+    A MetaLink4 object is created to contain a description of the
+    process output, and a MetaFile is created for each output file to be
+    appended to this link.
+
+    Parameters:
+        varname (str): Name of variable (used for MetaLink4)
+        desc (str): Description of meta file
+        outfiles (list): List of output files
+        format_name (str): Format name of output files
+        fmt (pywps.FORMATS): Format of output files
+        outdir (str): Directory containing output files
+
+    Returns:
+        MetaLink4.xml: xml of metalink connecting output files
+    """
+    if len(outfiles) == 1:
+        meta_link = MetaLink4(
+            "output", f"Output of {format_name} {varname} file", workdir=outdir
+        )
+    else:
+        meta_link = MetaLink4(
+            "output", f"Output of {format_name} {varname} files", workdir=outdir
+        )
+
+    for file in outfiles:
+        # Create a MetaFile instance, which instantiates a ComplexOutput object.
+        meta_file = MetaFile(f"{file}", desc, fmt=fmt)
+        meta_file.file = os.path.join(outdir, file)
+        meta_link.append(meta_file)
+
+    return meta_link.xml
+
+
+def copy_http_content(http, file):
+    """
+        This function is implemented to copy the content of a file passed
+        as an http address to a local file.
+
+        Parameters:
+            http (str): http address containing the desired content
+            file (file object): path to the 
+                file that the content will be copied to
+        Returns:
+            Path to the copied file in /tmp directory
+        """
+    file.write(get(http).content)
+    return file.name
